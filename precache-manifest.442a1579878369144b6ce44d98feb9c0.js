931ff2afda1f9e577e156ab41a9ead7e2301e41a
shilangyu <xmarcinmarcin@gmail.com>
2018-12-30 20:10:09 +0100

    Updates

---
+++ precache-manifest.442a1579878369144b6ce44d98feb9c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e4c6ed0e02ac39e8ded",
+    "url": "/pokego-trading-card/static/js/runtime~main.9e4c6ed0.js"
+  },
+  {
+    "revision": "3aef821498d0d4ae9318",
+    "url": "/pokego-trading-card/static/js/main.3aef8214.chunk.js"
+  },
+  {
+    "revision": "ecda0fd0d774b84d4982",
+    "url": "/pokego-trading-card/static/js/1.ecda0fd0.chunk.js"
+  },
+  {
+    "revision": "11b7e82a30a227102986691d97a6d6c8",
+    "url": "/pokego-trading-card/index.html"
+  }
+];
