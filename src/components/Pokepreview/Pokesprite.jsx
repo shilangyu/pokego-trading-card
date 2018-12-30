21576ceed5e4b40021bbd7705b2f7ff9c48c21a1
shilangyu <xmarcinmarcin@gmail.com>
2018-12-30 21:42:38 +0100

    error handling for image loading

--- src/components/Pokepreview/Pokesprite.jsx
+++ src/components/Pokepreview/Pokesprite.jsx
@@ -3,22 +3,25 @@ import { Image, Text } from 'react-konva'
 
 class Pokesprite extends Component {
 	state = {
-		image: null
+		image: undefined
 	}
 
 	componentDidMount() {
 		const image = new window.Image()
 		image.src = this.props.url
 		image.onload = () => this.setState({ image })
+		image.onerror = () => this.setState({ image: null })
 	}
 
 	render() {
 		const { x, y } = this.props
 
-		return this.state.image ? (
+		return this.state.image === undefined ? (
+			<Text text="Loading sprite..." x={x} y={y} />
+		) : this.state.image ? (
 			<Image image={this.state.image} x={x} y={y} />
 		) : (
-			<Text text="Loading sprite..." x={x} y={y} />
+			<Text text="Sprite not found!" x={x} y={y} />
 		)
 	}
 }
