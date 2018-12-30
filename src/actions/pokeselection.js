648aa4fa04ef1105f50a120230bfda970ac1170f
shilangyu <xmarcinmarcin@gmail.com>
2018-12-30 18:23:59 +0100

    refactored to async function

--- src/actions/pokeselection.js
+++ src/actions/pokeselection.js
@@ -3,19 +3,15 @@ import apiUrl from '../constants/api.json'
 
 export const loadPokemonList = {
 	type: 'LOAD_POKEMON_LIST',
-	payload: new Promise(async (resolve, reject) => {
-		try {
-			const calls = config.existingPokemon.generations.map(
-				e => apiUrl.base + apiUrl.generation.replace(':id', e)
-			)
-			const responses = await Promise.all(calls.map(e => fetch(e)))
-			const results = await Promise.all(responses.map(e => e.json()))
+	payload: (async () => {
+		const calls = config.existingPokemon.generations.map(
+			e => apiUrl.base + apiUrl.generation.replace(':id', e)
+		)
+		const responses = await Promise.all(calls.map(e => fetch(e)))
+		const results = await Promise.all(responses.map(e => e.json()))
 
-			return resolve(results)
-		} catch (err) {
-			return reject(err)
-		}
-	})
+		return results
+	})()
 }
 
 export const togglePokemonSelection = pokemonId => ({
