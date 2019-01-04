84ad03f9191bba4f2dbba48f554851e4afc92859
shilangyu <xmarcinmarcin@gmail.com>
2019-01-04 12:14:19 +0100

    Updates

--- precache-manifest.bf5b0e88ba0ce923ec428ced48e3e255.js
+++ precache-manifest.bf5b0e88ba0ce923ec428ced48e3e255.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/pokego-trading-card/static/js/runtime~main.9e4c6ed0.js"
   },
   {
-    "revision": "66538f9bce86411490c9",
-    "url": "/pokego-trading-card/static/js/main.66538f9b.chunk.js"
+    "revision": "dbaa7de564c1e04feae1",
+    "url": "/pokego-trading-card/static/js/main.dbaa7de5.chunk.js"
   },
   {
     "revision": "c6489c60c562215c11db",
     "url": "/pokego-trading-card/static/js/1.c6489c60.chunk.js"
   },
   {
-    "revision": "ca11526322acd3a85df686c91f64eec5",
+    "revision": "1ae92002da520f01c1a83c4005a286cb",
     "url": "/pokego-trading-card/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pokego-trading-card/precache-manifest.c576c42f8774b222652548806652a191.js"
+  "/pokego-trading-card/precache-manifest.bf5b0e88ba0ce923ec428ced48e3e255.js"
 );
 
 workbox.clientsClaim();
