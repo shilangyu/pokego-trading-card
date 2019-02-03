45f4e95ccc0a032f8dfa9ceca04aba5132860602
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2019-02-03 17:30:48 +0100

    created export button

--- src/components/Pokepreview/Pokemenu.jsx
+++ src/components/Pokepreview/Pokemenu.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import Button from '@material-ui/core/Button'
 import { withStyles } from '@material-ui/core/styles'
 import SaveIcon from '@material-ui/icons/Save'
+import KeyboardArrowUp from '@material-ui/icons/KeyboardArrowUp'
+
+import { getState } from '../../store'
 
 const styles = theme => ({
 	button: {
@@ -17,6 +20,17 @@ class Pokemenu extends Component {
 		// console.log(document.querySelector('canvas').toDataURL('image/png'))
 	}
 
+	onExportButtonClick(e) {
+		const { neededPokemons, offeredPokemons } = getState()
+		const exportData = { neededPokemons, offeredPokemons }
+
+		const a = document.createElement('a')
+		const file = new Blob([JSON.stringify(exportData)], { type: 'text/plain' })
+		a.href = URL.createObjectURL(file)
+		a.download = 'Exported-Trading-Card.json'
+		a.click()
+	}
+
 	render() {
 		const { classes } = this.props
 
@@ -25,6 +39,10 @@ class Pokemenu extends Component {
 				<Button variant="contained" className={classes.button} onClick={this.onSaveButtonClick}>
 					<SaveIcon className={classes.leftIcon} />
 					Save
+				</Button>
+				<Button variant="contained" className={classes.button} onClick={this.onExportButtonClick}>
+					<KeyboardArrowUp className={classes.leftIcon} />
+					Export
 				</Button>
 			</>
 		)
